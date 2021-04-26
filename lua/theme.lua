13ab0cba24291ba5ef6361f436b42a408e87e2b4
dgoncalves <dgoncalves@bsolus.pt>
2021-04-26 12:52:50 +0100

    Initial colors

---
+++ lua/theme.lua
@@ -0,0 +1,20 @@
+require("colorbuddy").setup()
+
+local Color = require('colorbuddy').Color
+
+Color.new('white',     '#e1e1e1')
+Color.new('red',       '#ff6666')
+Color.new('pink',      '#9e569b')
+Color.new('green',     '#88ff99')
+Color.new('yellow',    '#efef5a')
+Color.new('blue',      '#41a2fe')
+Color.new('aqua',      '#8ec0cc')
+Color.new('cyan',      '#8acec7')
+Color.new('purple',    '#8e6fad')
+Color.new('violet',    '#a284ab')
+Color.new('orange',    '#de935f')
+Color.new('brown',     '#a3785a')
+
+Color.new('seagreen',  '#89ab89')
+Color.new('turquoise', '#598b59')
+
